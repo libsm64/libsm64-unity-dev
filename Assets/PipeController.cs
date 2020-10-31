3ff217a09f2ebf02917c9b41a8f5bd068706e634
jaburns <j@jaburns.net>
2020-10-31 12:01:22 -0600

    Pipe scene

---
+++ Assets/PipeController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipeController : MonoBehaviour
+{
+    public GameObject marioPrefab;
+    public GameObject camObj;
+
+    IEnumerator Start()
+    {
+        while( true )
+        {
+            yield return new WaitForSeconds(1);
+            spawnMario();
+        }
+    }
+
+    void Update()
+    {
+        var r = 10;
+        var t = Time.time;
+
+        transform.position = new Vector3(
+            r * Mathf.Sin( t ),
+            transform.position.y,
+            r * Mathf.Cos( t )
+        );
+    }
+
+    void spawnMario()
+    {
+        var mario = Instantiate( marioPrefab, transform.position, Quaternion.identity );
+        mario.GetComponent<ExampleInputProvider>().cameraObject = camObj;
+    }
+}
---
+++ Assets/Spinner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    LibSM64.SM64DynamicTerrain terrain;
+
+    void Start()
+    {
+        terrain = GetComponent<LibSM64.SM64DynamicTerrain>();
+    }
+
+    void FixedUpdate()
+    {
+        terrain.SetRotation( Quaternion.AngleAxis( -20 * Time.time, Vector3.up ));
+    }
+}
